a486f9d6c153326cee70b9f9f03c8c0306589a7a
enrico <enrico>
2025-08-14 12:02:34 -0300

    novas rotas

--- index.js
+++ index.js
@@ -3,13 +3,45 @@ import express from 'express'
 const app = express()
 const PORT = 3000
 
+let usuarios = [];
+let contador =1;
+
 app.use(express.json())
 
 //ROTA DE TESTE
 app.get("/status", (req, res) => {
   res.json({message: "API Online"})
-})
+});
+
+//ROTA GET Listador todos o itens
+app.get("/usuarios", (req, res) => {
+  res.json(usuarios);
+});
+
+//ROTA POST Criar novo usuario
+app.post("/usuarios", (req, res) => {
+  const {nome} = req.body;
+  
+  const novoUsuario = {
+    id: contador++,
+    nome
+  };
+  
+  //ROTA PUT Atualiza usuarios por ID
+  app.put ("/usuarios/: id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const { nome } = req.body;
+
+    const usuario = usuarios.find((i) => i.id === id);
+
+    if(!usuario) {
+      return res.status(404).json({ erro: "Usuario não encontrado"});
+    }
+
+  usuarios.push(novoUsuario);
+  res.status(201).json(novoUsuario);
+});
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`)
-})
+});
